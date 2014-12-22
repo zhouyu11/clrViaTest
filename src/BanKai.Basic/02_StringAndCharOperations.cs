c94f3b70cacc5c75e24da38c7eec48f7b3f5174f
zhouyu <yuzhou@thoughtworks.com>
2014-12-23 00:19:38 +0800

    [zy] #StringAndCharOperation

--- src/BanKai.Basic/02_StringAndCharOperations.cs
+++ src/BanKai.Basic/02_StringAndCharOperations.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
 using Xunit;
 
@@ -16,7 +17,7 @@ namespace BanKai.Basic
             const string name = "Hall";
 
             // change "default(string)" to correct value.
-            const string expectedResult = default(string);
+            string expectedResult = String.Concat(title,name);
 
             Assert.Equal(expectedResult, (title + name));
         }
@@ -29,6 +30,8 @@ namespace BanKai.Basic
 
             var builder = new StringBuilder();
             // add at most 2 lines of code here concating variable "title" and "name".
+            builder.Append(title);
+            builder.Append(name);
 
             Assert.Equal("Mr. Hall", builder.ToString());
         }
@@ -40,8 +43,8 @@ namespace BanKai.Basic
             string replacement = originalString.Replace("Str", "W");
 
             // change "" in the following 2 lines to correct values.
-            const string expectedOrignalString = "";
-            const string expectedReplacement = "";
+            const string expectedOrignalString = "Original String";
+            const string expectedReplacement = "Original Wing";
             
             Assert.Equal(expectedOrignalString, originalString);
             Assert.Equal(expectedReplacement, replacement);
@@ -54,7 +57,7 @@ namespace BanKai.Basic
             builder.Replace("Str", "W");
 
             // change "" in the following line to correct value.
-            const string expectedResult = "";
+            const string expectedResult = "Original Wing";
 
             Assert.Equal(expectedResult, builder.ToString());
         }
@@ -66,7 +69,7 @@ namespace BanKai.Basic
             char characterAtIndex2 = originalString[2];
 
             // change "default(char)" to correct value.
-            const char expectedResult = default (char);
+            const char expectedResult = 'i';
 
             Assert.Equal(expectedResult, characterAtIndex2);
         }
@@ -78,7 +81,7 @@ namespace BanKai.Basic
             string equivalent = "Original" + " String";
 
             // change "default(bool)" to correct value.
-            const bool expectedResult = default(bool);
+            const bool expectedResult = true;
 
             Assert.Equal(expectedResult, (str == equivalent));
         }
@@ -90,8 +93,8 @@ namespace BanKai.Basic
             const string inDifferentCase = "oRiginal String";
 
             // change the variable values in the following 2 lines.
-            var caseSensitiveComparison = StringComparison.InvariantCultureIgnoreCase;
-            var caseInsensitiveComparison = StringComparison.InvariantCulture;
+            var caseSensitiveComparison = StringComparison.InvariantCulture;
+            var caseInsensitiveComparison = StringComparison.InvariantCultureIgnoreCase;
 
             Assert.False(originalString.Equals(inDifferentCase, caseSensitiveComparison));
             Assert.True(originalString.Equals(inDifferentCase, caseInsensitiveComparison));
